9d69b29539b4aa3435bbd1364bff5fd31c10dc59
ownhere <ownhere@gmail.com>
2011-10-13 09:54:45 +0800

    direct umount /data in checkdata.sh

--- initramfs.ownhere/ownhere/scripts/init_0checkdata.sh
+++ initramfs.ownhere/ownhere/scripts/init_0checkdata.sh
@@ -1,6 +1,7 @@
 #!/system/bin/sh
 
-mount -o remount,ro,nosuid,nodev,noatime,barrier=1,data=ordered,noauto_da_alloc,discard,commit=13 /dev/block/mmcblk0p10 /data
+#mount -o remount,ro,nosuid,nodev,noatime,barrier=1,data=ordered,noauto_da_alloc,discard,commit=13 /dev/block/mmcblk0p10 /data
+sync;sync;sync
 umount /data
 e2fsck -y /dev/block/mmcblk0p10 > /cache/checkdata.log 2>&1
 mount -t ext4 -o rw,nosuid,nodev,noatime,barrier=1,data=ordered,noauto_da_alloc,discard,commit=13 /dev/block/mmcblk0p10 /data
