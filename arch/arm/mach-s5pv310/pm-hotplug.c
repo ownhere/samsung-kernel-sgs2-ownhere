cf38bfffc898dfbcfda3229cfe7564c63377d77a
ownhere <ownhere@gmail.com>
2011-10-22 22:00:45 +0800

    Revert "pm-hotplug: disable secondary cpu auto-hotplug when screen is off" maybe lead to reboot unexpected.
    This reverts commit 2a12f7f7d8b1cf4975ed57582257557e756ed5cc.

--- arch/arm/mach-s5pv310/pm-hotplug.c
+++ arch/arm/mach-s5pv310/pm-hotplug.c
@@ -26,7 +26,6 @@
 #include <linux/sched.h>
 #include <linux/suspend.h>
 #include <linux/reboot.h>
-#include <linux/earlysuspend.h>
 
 #include <plat/map-base.h>
 #include <plat/gpio-cfg.h>
@@ -66,8 +65,6 @@ struct cpu_time_info {
 
 static DEFINE_PER_CPU(struct cpu_time_info, hotplug_cpu_time);
 
-static bool screen_off;
-
 /* mutex can be used since hotplug_timer does not run in
    timer(softirq) context but in process context */
 static DEFINE_MUTEX(hotplug_lock);
@@ -78,10 +75,6 @@ static void hotplug_timer(struct work_struct *work)
 
 	mutex_lock(&hotplug_lock);
 
-	if (screen_off && !cpu_online(1)) {
-		printk(KERN_INFO "pm-hotplug: disable cpu auto-hotplug\n");
-		goto no_hotplug;
-	}
 	if (user_lock == 1)
 		goto no_hotplug;
 
@@ -181,29 +174,6 @@ static struct notifier_block hotplug_reboot_notifier = {
 	.notifier_call = hotplug_reboot_notifier_call,
 };
 
-static void hotplug_early_suspend(struct early_suspend *handler)
-{
-	mutex_lock(&hotplug_lock);
-	screen_off = true;
-	mutex_unlock(&hotplug_lock);
-}
-
-static void hotplug_late_resume(struct early_suspend *handler)
-{
-	printk(KERN_INFO "pm-hotplug: enable cpu auto-hotplug\n");
-
-	mutex_lock(&hotplug_lock);
-	screen_off = false;
-	queue_delayed_work_on(0, hotplug_wq, &hotplug_work, hotpluging_rate);
-	mutex_unlock(&hotplug_lock);
-}
-
-static struct early_suspend hotplug_early_suspend_notifier = {
-	.suspend = hotplug_early_suspend,
-	.resume = hotplug_late_resume,
-	.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN,
-};
-
 static int __init s5pv310_pm_hotplug_init(void)
 {
 	printk(KERN_INFO "SMDKV310 PM-hotplug init function\n");
@@ -219,7 +189,6 @@ static int __init s5pv310_pm_hotplug_init(void)
 
 	register_pm_notifier(&s5pv310_pm_hotplug_notifier);
 	register_reboot_notifier(&hotplug_reboot_notifier);
-	register_early_suspend(&hotplug_early_suspend_notifier);
 
 	return 0;
 }
