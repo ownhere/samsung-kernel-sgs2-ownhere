d75e525fa243668c57fa304453ab21885a112551
ownhere <ownhere@gmail.com>
2011-12-17 22:25:39 +0800

    Revert "apply softirq patchs from 2.6.35.14, which resovled event/0 suspend problem."
    This reverts commit 759d2797e5e4c40b3805de76abd4f60c39f79349.
    
    which maybe leads to bluetooth receive issue.

--- net/sched/cls_cgroup.c
+++ net/sched/cls_cgroup.c
@@ -34,6 +34,8 @@ struct cgroup_subsys net_cls_subsys = {
 	.populate	= cgrp_populate,
 #ifdef CONFIG_NET_CLS_CGROUP
 	.subsys_id	= net_cls_subsys_id,
+#else
+#define net_cls_subsys_id net_cls_subsys.subsys_id
 #endif
 	.module		= THIS_MODULE,
 };
@@ -121,7 +123,7 @@ static int cls_cgroup_classify(struct sk_buff *skb, struct tcf_proto *tp,
 	 * calls by looking at the number of nested bh disable calls because
 	 * softirqs always disables bh.
 	 */
-	if (in_serving_softirq()) {
+	if (softirq_count() != SOFTIRQ_OFFSET) {
 		/* If there is an sk_classid we'll use that. */
 		if (!skb->sk)
 			return -1;
