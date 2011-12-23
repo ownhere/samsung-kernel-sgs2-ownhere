83d657c87d863374613f044aa9a82caacc7e4164
ownhere <ownhere@gmail.com>
2011-12-23 10:02:53 +0800

    Revert "set sched_smt_power_savings = 1, sched_mc_power_savings = 1;"
    This reverts commit ba0069c3d25a47dbccf140217f048fb2be06a447.
    
    try

--- kernel/sched.c
+++ kernel/sched.c
@@ -6676,7 +6676,7 @@ static void sched_domain_node_span(int node, struct cpumask *span)
 }
 #endif /* CONFIG_NUMA */
 
-int sched_smt_power_savings = 1, sched_mc_power_savings = 1;
+int sched_smt_power_savings = 0, sched_mc_power_savings = 0;
 
 /*
  * The cpus mask in sched_group and sched_domain hangs off the end.
