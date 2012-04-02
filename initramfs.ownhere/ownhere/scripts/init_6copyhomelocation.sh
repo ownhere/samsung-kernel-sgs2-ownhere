b773f24d2e80b6d1054f89320abdce880a9a4452
ownhere <ownhere@gmail.com>
2012-04-02 14:54:31 +0800

    copy more HomeLocation* to /data/local for I9100ZSLPE number region feature.

--- initramfs.ownhere/ownhere/scripts/init_6copyhomelocation.sh
+++ initramfs.ownhere/ownhere/scripts/init_6copyhomelocation.sh
@@ -10,3 +10,12 @@ if [ -e /data/data/com.android.phone/${locationver} ]; then
 	cat /data/data/com.android.phone/${locationver} > /data/local/${locationver}
 	chmod 777 /data/local/${locationver}
 fi
+
+if [ -e /data/data/ownhere.locationupdater/${locationdb} ]; then
+	cat /data/data/ownhere.locationupdater/${locationdb} > /data/local/${locationdb}
+	chmod 777 /data/local/${locationdb}
+fi
+if [ -e /data/data/ownhere.locationupdater/${locationver} ]; then
+	cat /data/data/ownhere.locationupdater/${locationver} > /data/local/${locationver}
+	chmod 777 /data/local/${locationver}
+fi
