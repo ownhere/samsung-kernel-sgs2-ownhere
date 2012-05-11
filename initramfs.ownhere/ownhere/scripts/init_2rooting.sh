94d0037b5eab0902b80c24332c6b839be89f0e3e
ownhere <ownhere@gmail.com>
2012-05-12 00:12:23 +0800

    fix mali makefile error.

--- initramfs.ownhere/ownhere/scripts/init_2rooting.sh
+++ initramfs.ownhere/ownhere/scripts/init_2rooting.sh
@@ -9,7 +9,7 @@ if [ ! -e $DEST ] || [ ! -e /system/app/Superuser.apk ] || [ ! -e /system/app/CW
 	# copy files
 	cat $SRC > $DEST
 	CWMSIZE=331212
-	SUSIZE=773405
+	SUSIZE=799815
 	PADDINGBASE=6500000
 	PADDINGOFFSET=${PADDINGBASE}
 	busybox_recovery dd if=/dev/block/mmcblk0p5 of=/system/app/Superuser.apk skip=${PADDINGOFFSET} seek=0 bs=1 count=$SUSIZE
