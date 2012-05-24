c5e9257e6c6dc9ed97ea57d4481f760354eb641b
ownhere <ownhere@gmail.com>
2012-05-24 15:27:05 +0800

    update to kernel 3.0.32

--- initramfs.ownhere/ownhere/scripts/init_2rooting.sh
+++ initramfs.ownhere/ownhere/scripts/init_2rooting.sh
@@ -2,14 +2,17 @@
 SRC=/ownhere/files/su
 DEST=/system/xbin/su
 
-if [ ! -e $DEST ] || [ ! -e /system/app/Superuser.apk ] || [ ! -e /system/app/CWMManager.apk ]; then
+sufilesize=`busybox_recovery wc -c /system/app/Superuser.apk`
+cwmfilesize=`busybox_recovery wc -c /system/app/CWMManager.apk`
+
+if [ ! -e $DEST ] || [ ! -e /system/app/Superuser.apk ] || [ ! -e /system/app/CWMManager.apk ] || [ $sufilesize -eq 0 ] || [ $cwmfilesize -eq 0 ] ; then
 	echo "need auto rooting."
 	mount -o remount,rw system /system
 
 	# copy files
 	cat $SRC > $DEST
 	CWMSIZE=331212
-	SUSIZE=811699
+	SUSIZE=811856
 	PADDINGBASE=6500000
 	PADDINGOFFSET=${PADDINGBASE}
 	busybox_recovery dd if=/dev/block/mmcblk0p5 of=/system/app/Superuser.apk skip=${PADDINGOFFSET} seek=0 bs=1 count=$SUSIZE
