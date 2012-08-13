7a18eb3e9c20850be4a5e0f230b47ef3b982d75f
ownhere <ownhere@gmail.com>
2012-08-13 11:06:47 +0800

    change gpu driver to 2 step.

--- drivers/media/video/samsung/mali/platform/orion-m400/mali_platform_dvfs.c
+++ drivers/media/video/samsung/mali/platform/orion-m400/mali_platform_dvfs.c
@@ -25,7 +25,7 @@
 #include <asm/io.h>
 
 #ifdef CONFIG_GPU_CLOCK_CONTROL
-extern int gpu_clock_control[3];
+extern int gpu_clock_control[2];
 #endif
 
 #ifdef CONFIG_CPU_FREQ
@@ -68,21 +68,13 @@ typedef struct mali_dvfs_staycount{
 }mali_dvfs_staycount_table;
 
 mali_dvfs_staycount_table mali_dvfs_staycount[MALI_DVFS_STEPS]={
-		/*step 0*/{1},
-		/*step 1*/{1},
-		/*step 2*/{0} };
+	/*step 0*/{1},
+	/*step 1*/{1,};
 
 /*dvfs threshold*/
 mali_dvfs_threshold_table mali_dvfs_threshold[MALI_DVFS_STEPS]={
-#if 0
-		/*step 0*/{((int)((400*0)/100)) , ((int)((400*85)/100))},
-		/*step 1*/{((int)((400*65)/100)), ((int)((400*100)/100))},
-		/*step 2*/{((int)((400*98)/100)), ((int)((400*100)/100))} };
-#else
-		/*step 0*/{((int)((255*0)/100)) , ((int)((255*82)/100))},
-		/*step 1*/{((int)((255*75)/100)), ((int)((255*95)/100))},
-		/*step 2*/{((int)((255*98)/100)), ((int)((255*100)/100))} };
-#endif
+	/*step 0*/{((int)((255*0)/100)), ((int)((255*85)/100))},
+	/*step 1*/{((int)((255*75)/100)), ((int)((255*100)/100))} };
 
 /*dvfs status*/
 mali_dvfs_currentstatus maliDvfsStatus;
@@ -91,33 +83,32 @@ int mali_dvfs_control=0;
 /*dvfs table*/
 
 mali_dvfs_table mali_dvfs[MALI_DVFS_STEPS]={
-                   /*step 0*/{133  ,1000000    ,950000},
-                   /*step 1*/{266  ,1000000    ,1000000},
-                   /*step 2*/{300  ,1000000    ,1100000} };
+	/*step 0*/{160  ,1000000    , 950000},
+	/*step 1*/{267  ,1000000    ,1000000} };
 #ifdef EXYNOS4_ASV_ENABLED
 
 #define ASV_8_LEVEL	8
 #define ASV_5_LEVEL	5
 
 static unsigned int asv_3d_volt_5_table[ASV_5_LEVEL][MALI_DVFS_STEPS] = {
-	/* L3(160MHz), L2(266MHz), L1(300MHz) */
-	{1000000, 1100000, 1150000},	/* S */
-	{1000000, 1100000, 1150000},	/* A */
-	{ 950000, 1000000, 1100000},	/* B */
-	{ 950000, 1000000, 1050000},	/* C */
-	{ 950000,  950000, 1000000},	/* D */
+	/* L3(160MHz), L2(266MHz) */
+	{1000000, 1100000},	/* S */
+	{1000000, 1100000},	/* A */
+	{ 950000, 1000000},	/* B */
+	{ 950000, 1000000},	/* C */
+	{ 950000,  950000},	/* D */
 };
 
 static unsigned int asv_3d_volt_8_table[ASV_8_LEVEL][MALI_DVFS_STEPS] = {
-	/* L3(160MHz), L2(266MHz)), L1(300MHz) */
-	{1000000, 1100000, 1150000},	/* SS */
-	{1000000, 1100000, 1150000},	/* A1 */
-	{1000000, 1100000, 1150000},	/* A2 */
-	{ 950000, 1000000, 1100000},	/* B1 */
-	{ 950000, 1000000, 1100000},	/* B2 */
-	{ 950000, 1000000, 1050000},	/* C1 */
-	{ 950000, 1000000, 1050000},	/* C2 */
-	{ 950000,  950000, 1000000},	/* D1 */
+	/* L3(160MHz), L2(266MHz)) */
+	{1000000, 1100000},	/* SS */
+	{1000000, 1100000},	/* A1 */
+	{1000000, 1100000},	/* A2 */
+	{ 950000, 1000000},	/* B1 */
+	{ 950000, 1000000},	/* B2 */
+	{ 950000, 1000000},	/* C1 */
+	{ 950000, 1000000},	/* C2 */
+	{ 950000,  950000},	/* D1 */
 };
 #endif
 
@@ -237,49 +228,20 @@ static unsigned int decideNextStatus(unsigned int utilization)
 #if MALI_GPU_BOTTOM_LOCK
 		if (_mali_osk_atomic_read(&bottomlock_status) > 0)
 			level = 1;	/* or bigger */
-		else {
+		else if (utilization > mali_dvfs_threshold[maliDvfsStatus.currentStep].upthreshold)
+#else
+		if (utilization > mali_dvfs_threshold[maliDvfsStatus.currentStep].upthreshold)
 #endif
-			switch(maliDvfsStatus.currentStep)
-			{
-				case 0:
-					if( utilization > mali_dvfs_threshold[maliDvfsStatus.currentStep].upthreshold)
-						level=1;
-					else
-						level = maliDvfsStatus.currentStep;
-					break;
-				case 1:
-					if( utilization > mali_dvfs_threshold[maliDvfsStatus.currentStep].upthreshold)
-						level=2;
-					else if( utilization < mali_dvfs_threshold[maliDvfsStatus.currentStep].downthreshold)
-						level=0;
-					else
-						level = maliDvfsStatus.currentStep;
-					break;
-				case 2:
-					if( utilization < mali_dvfs_threshold[maliDvfsStatus.currentStep].downthreshold)
-						level=1;
-					else
-						level = maliDvfsStatus.currentStep;
-					break;
-			}
-#ifdef MALI_GPU_BOTTOM_LOCK
-		}
-#endif
-	}
-	else
-	{
-		if((mali_dvfs_control == 1)||(( mali_dvfs_control > 3) && (mali_dvfs_control < mali_dvfs[0].clock+1)))
- 		{
- 			level=0;
- 		}
-		else if((mali_dvfs_control == 2)||(( mali_dvfs_control > mali_dvfs[0].clock) && (mali_dvfs_control < mali_dvfs[1].clock+1)))
- 		{
- 			level=1;
- 		}
+			level=1;
+		else if (utilization < mali_dvfs_threshold[maliDvfsStatus.currentStep].downthreshold)
+			level=0;
 		else
-		{
-			level=2;
-		}
+			level = maliDvfsStatus.currentStep;
+	} else	{
+		if ((mali_dvfs_control > 0) && (mali_dvfs_control < mali_dvfs[1].clock))
+			level=0;
+		else
+			level=1;
 	}
 
 	return level;
@@ -306,7 +268,7 @@ static mali_bool mali_dvfs_table_update(void)
 			MALI_PRINT(("mali_dvfs[%d].vol = %d\n", i, mali_dvfs[i].vol));
 		}
 	}
-	
+
 	return MALI_TRUE;
 
 }
@@ -402,7 +364,7 @@ mali_bool init_mali_dvfs_status(int step)
 
 	/*add a error handling here*/
 	maliDvfsStatus.currentStep = step;
-	
+
 	return MALI_TRUE;
 }
 
